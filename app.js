03c9df882b79cb2ecffc9565d3af93a57badb6d7
MartBuf <marrtinbuf@gmail.com>
2022-06-21 21:08:59 -0300

    aa

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require ('express') ;
-const app = express (); 
-const path = require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
 
+const publicPath = path.resolve(__dirname, "./public");
 
+app.use(express.static(publicPath));
 
-const publicPath = path.resolve(__dirname,"./public")
-app.use(express.static(publicPath))
-app.get("/",(req,res)=> {res.sendFile(path.resolve(__dirname,"./views/home.html"))});
-app.get("/register",(req,res)=> {res.sendFile(path.resolve(__dirname,"./views/register.html"))});
-app.get("/login",(req,res)=> {res.sendFile(path.resolve(__dirname,"./views/login.html"))});
-app.listen(process.env.PORT || 4000 , ()=>console.log('Servidor de Prueba'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+app.listen(process.env.PORT || 4000, () => console.log("Servidor de Prueba"));
