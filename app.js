a7287c82cf13f1716bf52c58cc388a515835f7c9
MartBuf <marrtinbuf@gmail.com>
2022-06-21 21:03:18 -0300

    PP

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const express = require ('express') ;
+const app = express (); 
+const path = require('path');
 
-const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || 'localhost';
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-
-app.listen(4000, ()=>{
-    console.log(`Server running at http://localhost:4000/`);
-});
+const publicPath = path.resolve(__dirname,"./public")
+app.use(express.static(publicPath))
+app.get("/",(req,res)=> {res.sendFile(path.resolve(__dirname,"./views/home.html"))});
+app.get("/register",(req,res)=> {res.sendFile(path.resolve(__dirname,"./views/register.html"))});
+app.get("/login",(req,res)=> {res.sendFile(path.resolve(__dirname,"./views/login.html"))});
+app.listen(process.env.PORT || 4000 , ()=>console.log('Servidor de Prueba'));
